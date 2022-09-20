baade4581129fc41793ceea7bd4a4ee7217ad007
luoyongzhi <lyz17693106715@163.com>
2022-09-20 21:00:18 +0800

    基础搭建

---
+++ ins-user-internal/src/main/java/com/shestnut/user/internal/user/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.shestnut.user.internal.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author luoyongzhi
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+}
